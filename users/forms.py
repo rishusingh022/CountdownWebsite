9edc9624b8c1113101805afcb45baef1dd53ca2a
rishusingh022 <rishusingh022@gmail.com>
2020-10-29 02:51:27 +0530

    intial setup

---
+++ users/forms.py
@@ -0,0 +1,58 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+	email = forms.EmailField(
+		label='',
+		max_length=254,
+		widget=forms.EmailInput(
+			attrs={
+				"placeholder": "Email",
+				"class": "form-control"
+			}
+		)
+	)
+
+	username = forms.CharField(
+		label='',
+		max_length=30,
+		min_length=5,
+		required=True,
+		widget=forms.TextInput(
+			attrs={
+				"placeholder": "Username",
+				"class": "form-control"
+			}
+		)
+	)
+
+	password1 = forms.CharField(
+		label='',
+		max_length=30,
+		min_length=8,
+		required=True,
+		widget=forms.PasswordInput(
+			attrs={
+				"placeholder": "Password",
+				"class": "form-control"
+			}
+		)
+	)
+
+	password2 = forms.CharField(
+		label='',
+		max_length=30,
+		min_length=8,
+		required=True,
+		widget=forms.PasswordInput(
+			attrs={
+				"placeholder": "Confirm Password",
+				"class": "form-control"
+			}
+		)
+	)
+	
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password1', 'password2')
